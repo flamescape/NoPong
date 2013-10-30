c7c22b8dc63df15773e7cb6951f8b4342d00fbde
Mikuso <g_github@flamescape.com>
2013-10-30 21:39:35 +0000

    More stuff

--- client/js/game.js
+++ client/js/game.js
@@ -1,3 +1,3 @@
 
+var socket = io.connect();
 
-// do game stuff here
--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require('colors')
+
 var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
@@ -9,6 +11,7 @@ server.listen(8080);
 app.use(express.static('../client'));
 
 io.sockets.on('connection', function(sock) {
+    console.log('New client connected. Waiting for game.');
     sock.join('waitingForGame');
 });
 
@@ -20,10 +23,12 @@ var gameCounter = 0;
 setInterval(function(){
     var waitingClients = io.sockets.clients('waitingForGame');
     while (waitingClients.length > 1) {
+        console.log('Found 2 clients - '+('matchmaking!'.blue));
         gameCounter++;
         waitingClients.slice(0, 1).forEach(function(wc){
             wc.leave('waitingForGame');
             wc.join('game.'+gameCounter);
+            console.log();
         });
     }
 }, 2000);

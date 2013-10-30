de086a76f5d3b32c2cf3b1e40a52cc5662d17796
John Ballantyne <johnballantyne@gmail.com>
2013-10-30 17:48:12 -0400

    Add a pink rectangle

--- client/js/game.js
+++ client/js/game.js
@@ -1,3 +1,24 @@
 
 var socket = io.connect();
 
+var stage = new Kinetic.Stage({
+    container: 'container',
+    width: 578,
+    height: 200
+});
+
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+    x: 239,
+    y: 75,
+    width: 100,
+    height: 50,
+    fill: 'pink',
+    stroke: 'black',
+    strokewidth: 1
+});
+
+layer.add(rect);
+
+stage.add(layer);

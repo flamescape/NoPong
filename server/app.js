2e8c328d69eae89ffc9b4d77a14753d3376e6f48
Mikuso <g_github@flamescape.com>
2013-10-30 21:29:30 +0000

    Add a basic waiting area

--- server/app.js
+++ server/app.js
@@ -4,10 +4,26 @@ var express = require('express')
   , io = require('socket.io').listen(server)
   ;
 
-server.listen(80);
+server.listen(8080);
 
 app.use(express.static('../client'));
 
 io.sockets.on('connection', function(sock) {
-
+    sock.join('waitingForGame');
 });
+
+
+var runningGames = [];
+
+// matchmaker
+var gameCounter = 0;
+setInterval(function(){
+    var waitingClients = io.sockets.clients('waitingForGame');
+    while (waitingClients.length > 1) {
+        gameCounter++;
+        waitingClients.slice(0, 1).forEach(function(wc){
+            wc.leave('waitingForGame');
+            wc.join('game.'+gameCounter);
+        });
+    }
+}, 2000);

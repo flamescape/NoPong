5986d31da1a895e6ef8133b8f498fb428d971df7
Mikuso <g_github@flamescape.com>
2013-10-30 21:55:42 +0000

    Change some things, or whatever.

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ require('colors')
 var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
+  , io = require('socket.io').listen(server, {log:false})
   ;
 
 server.listen(8080);
@@ -21,14 +21,18 @@ var runningGames = [];
 // matchmaker
 var gameCounter = 0;
 setInterval(function(){
-    var waitingClients = io.sockets.clients('waitingForGame');
-    while (waitingClients.length > 1) {
-        console.log('Found 2 clients - '+('matchmaking!'.blue));
+    while (true) {
+        var waitingClients = io.sockets.clients('waitingForGame')
+        if (waitingClients.length < 2)
+            break;
+    
+        console.log('Found 2 clients - '+('matchmaking!'.red));
         gameCounter++;
-        waitingClients.slice(0, 1).forEach(function(wc){
+        waitingClients.slice(0, 2).forEach(function(wc, idx){
             wc.leave('waitingForGame');
             wc.join('game.'+gameCounter);
-            console.log();
+            console.log('Player '+(idx+1)+': '+(wc.handshake.address.address.cyan));
         });
+        console.log('Fight!!'.yellow);
     }
 }, 2000);

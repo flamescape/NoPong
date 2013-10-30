ecd470e27bc3b62fc19c2768889c2af02518d054
Mikuso <g_github@flamescape.com>
2013-10-30 21:14:59 +0000

    Add app.js

---
+++ server/app.js
@@ -0,0 +1,11 @@
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(80);
+
+app.use(express.static('../client'));
+
+io.sockets.on('connection', function(sock) {
+
+});

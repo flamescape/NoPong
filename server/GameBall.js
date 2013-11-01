97346f1447de3c15b097aca0030e8f109bee6a74
Mikuso <g_github@flamescape.com>
2013-11-01 01:19:28 +0000

    Add some messy wall collision detection.

--- server/GameBall.js
+++ server/GameBall.js
@@ -10,46 +10,78 @@ var GameBall = function() {
     });
     this.paddles = [];
 };
-GameBall.prototype.addCollidingPaddle = function(paddle) {
-    this.paddles.push(paddle);
-    return this;
-};
-GameBall.prototype.update = function(data) {
-    _.isObject(data) && _.extend(this, data || {});
-    return this;
-};
-GameBall.prototype.calcDelta = function(){
-    var delta;
-    if (this.prevTick) {
-        delta = Date.now() - this.prevTick;
-    }
-    this.prevTick = Date.now();
-    return delta && delta / (1000/this.fps);
-};
-GameBall.prototype.tick = function(){
-    var delta = this.calcDelta();
-    if (!delta) {
-        // delta === 0 ? no time has passed.
-        // delta === null ? nothing to calculate.
-        return;
-    }        
+GameBall.prototype = {
 
-    while (this.angle >= Math.PI * 2) { this.angle -= (Math.PI * 2); }
-    while (this.angle < 0) { this.angle += (Math.PI * 2); }
+    addCollidingPaddle: function(paddle) {
+        this.paddles.push(paddle);
+        return this;
+    },
     
-    var xm = Math.cos(this.angle) * this.speed * delta;
-    var ym = Math.sin(this.angle) * this.speed * delta;
+    update: function(data) {
+        _.isObject(data) && _.extend(this, data || {});
+        return this;
+    },
     
-    // check for paddle collisions
-    this.paddles.forEach(function(){
-        // todo: see comment ^
-    });
+    calcDelta: function(){
+        var delta;
+        if (this.prevTick) {
+            delta = Date.now() - this.prevTick;
+        }
+        this.prevTick = Date.now();
+        return delta && delta / (1000/this.fps);
+    },
     
-    // check for wall collisions
-    // todo: see comment ^
-    
-    this.x += xm;
-    this.y += ym;
+    tick: function(){
+        var delta = this.calcDelta();
+        if (!delta) {
+            // delta === 0 ? no time has passed.
+            // delta === null ? nothing to calculate.
+            return;
+        }        
+
+        while (this.angle > Math.PI) { this.angle -= Math.PI; }
+        while (this.angle < -Math.PI) { this.angle += Math.PI; }
+        
+        var xm = Math.cos(this.angle) * this.speed * delta;
+        var ym = Math.sin(this.angle) * this.speed * delta;
+        
+        // check for paddle collisions
+        this.paddles.forEach(function(){
+            // todo: see comment ^
+        });
+        
+        // check for wall collisions
+        if (this.y + ym > 1) {
+            var overshoot = (this.y + ym) - 1;
+            var close = 1 - this.y;
+            ym = close - overshoot;
+            this.angle = -this.angle;
+        }
+        
+        if (this.x + xm > 1) {
+            var overshoot = (this.x + xm) - 1;
+            var close = 1 - this.x;
+            xm = close - overshoot;
+            this.angle = (this.angle >= 0 ? Math.PI : -Math.PI) - this.angle;
+        }
+        
+        if (this.y + ym < 0) {
+            var overshoot = (this.y + ym) - 0;
+            var close = 0 - this.y;
+            ym = close - overshoot;
+            this.angle = -this.angle;
+        }
+        
+        if (this.x + xm < 0) {
+            var overshoot = (this.x + xm) - 0;
+            var close = 0 - this.x;
+            xm = close - overshoot;
+            this.angle = (this.angle >= 0 ? Math.PI : -Math.PI) - this.angle;
+        }
+        
+        this.x += xm;
+        this.y += ym;
+    }
 };
 
 typeof module !== 'undefined' && (module.exports = GameBall);

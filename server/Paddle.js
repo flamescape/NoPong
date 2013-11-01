4e327cb49fac4576298bec4bb9e254f34f89f9d5
John Ballantyne <johnballantyne@gmail.com>
2013-11-01 02:05:54 -0400

    Some Paddle stuff

--- server/Paddle.js
+++ server/Paddle.js
@@ -1,10 +1,10 @@
 var _ = _ || require('underscore');
 
-var Paddle = function() {
+var Paddle = function(x) {
     _.defaults(this, {
         height:  2/15,  //0.13333 repeating of course
         width:  3/160,  //0.01875
-        x: null,
+        x: x,
         y: 0.5
     });
 };
@@ -18,4 +18,18 @@ Paddle.prototype.clamp = function() {
     } else if (y < topBound) {
         y = topBound;
     }
+};
+
+Paddle.prototype.rect = new Kinetic.Rect({
+    x: null,
+    y: null,
+    width: null,
+    height: null,
+    fill: '#FFF',
+});
+
+Paddle.prototype.resize = function(w, h) {
+    this.rect.setWidth(this.width * w);
+    this.rect.setHeight(this.height * h);
+    //p.rect.setX((p.x - (p.width / 2)) * gWidth);
 };
